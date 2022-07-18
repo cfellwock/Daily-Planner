b1ecf4b273c90d9138feca706f0670e78cbbbb4b
Christian Fellwock <christian.kucoding@gmail.com>
2022-07-17 23:28:31 -0500

    Created timeKeeper function in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,32 @@
 // WHEN I open the planner, the current day is displayed at the top of the calendar
 let today = moment()
     $("#currentDay").text("Today is " + today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    today.setInterval(update, 1000);
+    // today.setInterval(update, 1000);
 
-
-
-
-// WHEN I scroll down, I am presented with time blocks for standard business hours
 
 // WHEN I view the time blocks for that day, each time block is color-coded to indicate whether it is in the past, present, or future
 
-// WHEN I click into a time block, I can enter an event
+
+function timeKeeper() {
+let currentHour = moment().hour();
+
+    let blockHour = [];
+    $('.row').each(function() {
+        if($(this).hasClass('time-block')) {
+            blockHour.push($(this).prop('id').split('hour')[1]);
+        }
+    });
+    console.log(blockHour)
+
+    if (currentHour < blockHour) {
+        $('.time-block').toggleClass("past")
+
+    }
+
+};
+
+// can i add a filler class into this to avoid writing 9 lines of code?
 
 // WHEN I click the save button for that time block, the text for that event is saved in local storage
 
 // WHEN I refresh the page, the saved events persist
-update();
